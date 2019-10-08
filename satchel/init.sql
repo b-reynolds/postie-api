3ed1aea392568f7a3e6e46195cd6d1d3970907e5
Ben Reynolds <breynolds1994@outlook.com>
2019-10-08 22:39:08 +0100

    Create table for storing files

--- satchel/init.sql
+++ satchel/init.sql
@@ -11,3 +11,18 @@ create unique index file_types_name_uindex
 	on file_types (name);
 
 insert into file_types (id, name, created_at) values (default, 'text', default);
+
+create table files
+(
+    id serial
+        constraint files_pk
+            primary key,
+    name text not null,
+    file_type_id int not null
+        constraint files_file_types_id_fk
+            references file_types,
+    contents text not null,
+    expired boolean default false not null,
+    created_at timestamptz default now() not null,
+    expires_at timestamptz
+);
