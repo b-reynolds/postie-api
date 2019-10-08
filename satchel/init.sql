e718a802cd05e6b1a7f10fccdaa8c67686681852
Ben Reynolds <breynolds1994@outlook.com>
2019-10-08 22:38:57 +0100

    Add timestamp column to the file types table

--- satchel/init.sql
+++ satchel/init.sql
@@ -3,10 +3,11 @@ create table file_types
 	id serial
 		constraint file_types_pk
 			primary key,
-	name text not null
+	name text not null,
+	created_at timestamptz default now() not null
 );
 
 create unique index file_types_name_uindex
 	on file_types (name);
 
-insert into file_types (id, name) values (default, 'text');
+insert into file_types (id, name, created_at) values (default, 'text', default);
