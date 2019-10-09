95c2cb237a86e4dcc588e773d14d6debc0c6ad99
Ben Reynolds <breynolds1994@outlook.com>
2019-10-09 19:49:58 +0100

    Create DAO for ac

---
+++ postroom/src/main/kotlin/satchel/Satchel.kt
@@ -0,0 +1,24 @@
+package satchel
+
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
+
+private const val DB_HOST = "jdbc:postgresql://satchel"
+private const val DB_PORT = 5432
+private const val DB_USER = "postgres"
+
+/**
+ * Satchel
+ */
+object Satchel {
+    /**
+     * Connects to and returns the Satchel data source.
+     */
+    fun connect(): Jdbi {
+        return Jdbi.create("$DB_HOST:$DB_PORT/?user=$DB_USER").apply {
+            installPlugin(KotlinSqlObjectPlugin())
+            installPlugin(KotlinPlugin())
+        }
+    }
+}
