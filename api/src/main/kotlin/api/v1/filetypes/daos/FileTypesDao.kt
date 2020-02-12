242d4fa1fe788ea28cf797dc8bd18ad4af4cfd19
Ben Reynolds <breynolds1994@outlook.com>
2020-02-12 20:27:26 +0000

    📦 new: Create DAO methods for retrieving file types

--- api/src/main/kotlin/api/v1/filetypes/daos/FileTypesDao.kt
+++ api/src/main/kotlin/api/v1/filetypes/daos/FileTypesDao.kt
@@ -1,5 +1,8 @@
 package api.v1.filetypes.daos
 
+import api.v1.files.models.File
+import api.v1.filetypes.models.FileType
+import org.jdbi.v3.sqlobject.kotlin.RegisterKotlinMapper
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 
 /**
@@ -11,4 +14,18 @@ interface FileTypesDao {
      */
     @SqlQuery("select exists(select 1 from file_types where id = ?)")
     fun contains(id: Int): Boolean
+
+    /**
+     * Returns all file types.
+     */
+    @SqlQuery("select * from file_types")
+    @RegisterKotlinMapper(File::class)
+    fun get(): List<FileType>
+
+    /**
+     * Returns the file type associated with the specified [id] or `null` if one does not exist.
+     */
+    @SqlQuery("select * from file_types where id = ?")
+    @RegisterKotlinMapper(File::class)
+    fun get(id: Int): FileType?
 }
